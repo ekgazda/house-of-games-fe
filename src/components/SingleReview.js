ce916cec3fb028ac1e5926eb67551700663ff916
Eniko Gazda <89908056+ekgazda@users.noreply.github.com>
2021-11-24 20:48:05 +0000

    add moment package

--- src/components/SingleReview.js
+++ src/components/SingleReview.js
@@ -2,6 +2,8 @@ import { getReviewById } from '../utils/api'
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import Nav from './Nav'
+import Comments from './Comments'
+import moment from 'moment'
 
 const SingleReview = () => {
   const { id } = useParams()
@@ -17,18 +19,21 @@ const SingleReview = () => {
     <>
       <Nav />
       <div className="SingleReview">
-        <div className="SingleReview">
-          <p><b>{review.title}</b></p>
-          <p>designer: {review.designer}</p>
-          <p>category: {review.category}</p>
-          <img src={review.review_img_url} alt="Review" className="ReviewImg" />
-          <p>{review.review_body}</p>
-          <p>posted: {review.created_at}</p>
-          <p>author: {review.owner}</p>
-          <p>votes: {review.votes}</p>
-          <p>comments: {review.comment_count}</p>
-        </div>
+        <p>
+          <b>{review.title}</b>
+        </p>
+        <p><i>designer:</i> {review.designer}</p>
+        <p><i>category:</i> {review.category}</p>
+        <img src={review.review_img_url} alt="Review" className="ReviewImg" />
+        <p>{review.review_body}</p>
+        <p><i>posted:</i> {moment(review.created_at).format("YYYY-MM-DD HH:mm")}</p>
+        <p><i>author:</i> {review.owner}</p>
+        <p>
+        <i>votes:</i> {review.votes} <button className="VoteBtn">Vote up</button>{' '}
+          <button className="VoteBtn">Vote down</button>{' '}
+        </p>
       </div>
+      <Comments />
     </>
   )
 }
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,12 +1,22 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 
 export const UserContext = createContext()
 
 export const UserProvider = ({children}) => {
-  const [currentUser, setCurrentUser] = useState({})
+  const [currentUser, setCurrentUser] = useState('')
+
+  useEffect(() => {
+    const user = localStorage.getItem('user')
+    setCurrentUser(user)
+  }, [setCurrentUser])
+
+  const updateCurrentUser = (user) => {
+    localStorage.setItem('user', user);
+    setCurrentUser(user)
+  }
 
   return (
-    <UserContext.Provider value={{ currentUser, setCurrentUser }}>
+    <UserContext.Provider value={{ currentUser, setCurrentUser: updateCurrentUser }}>
       {children}
     </UserContext.Provider>
   )
