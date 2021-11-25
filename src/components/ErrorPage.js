58036f457bff1e49e19f4cb718845fb6ee0529e6
Eniko Gazda <89908056+ekgazda@users.noreply.github.com>
2021-11-25 20:56:44 +0000

    add error for invalid url

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Nav from './Nav'
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <Nav />
+            <h3 id="errorPage">Oh-oh this page does not exist... </h3>
+        </div>
+    )
+}
+export default ErrorPage
