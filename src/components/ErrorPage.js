96f5e7f3d5a0845a61585b7344a3f82fae56ae9e
Eniko Gazda <89908056+ekgazda@users.noreply.github.com>
2021-11-26 15:42:33 +0000

    update 404 handling

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Nav from './Nav'
+import msg404Png from '../img/msg404.png'
 
 const ErrorPage = () => {
     return (
         <div>
             <Nav />
             <h3 id="errorPage">Oh-oh this page does not exist... </h3>
+            <img src={msg404Png} alt="404error" className="404error"></img>
         </div>
     )
 }
