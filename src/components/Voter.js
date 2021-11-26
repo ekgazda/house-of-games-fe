b021a69691930a46e9060af2d845d1112e5f54bf
Eniko Gazda <89908056+ekgazda@users.noreply.github.com>
2021-11-26 17:53:56 +0000

    uadd disable voting after one vote

--- src/components/Voter.js
+++ src/components/Voter.js
@@ -2,30 +2,42 @@ import { useState, useContext } from 'react'
 import { UserContext } from '../contexts/UserContext'
 import { updateVotesOnReviewById } from '../utils/api'
 
-const Voter = ({reviewId, votes, author}) => {
+const Voter = ({ reviewId, votes, author }) => {
   const [addedVotes, setAddedVotes] = useState(0)
+  const [voted, setVoted] = useState(false)
   const { currentUser } = useContext(UserContext)
 
   const addVotes = () => {
-    updateVotesOnReviewById(reviewId, 1).then(() => {
-      setAddedVotes(addedVotes => addedVotes + 1)
-    })
+    updateVotesOnReviewById(reviewId, 1).catch()
+    setAddedVotes((addedVotes) => addedVotes + 1)
+    setVoted(true)
   }
 
   const removeVotes = () => {
-    updateVotesOnReviewById(reviewId, -1).then(() => {
-      setAddedVotes(addedVotes => addedVotes - 1)
-    })
+    updateVotesOnReviewById(reviewId, -1).catch()
+    setAddedVotes((addedVotes) => addedVotes - 1)
+    setVoted(true)
   }
 
   return (
     <>
       <i>votes:</i> {votes + addedVotes}
-      {author !== currentUser && <button className="VoteBtn" onClick={addVotes}>Vote up</button>}
-      {author !== currentUser && <button className="VoteBtn" onClick={removeVotes}>Vote down</button>}
+      {author !== currentUser ? (
+        <>
+          <button className="VoteBtn" onClick={addVotes} disabled={voted === true}>
+            Vote up
+          </button>
+          <button className="VoteBtn" onClick={removeVotes} disabled={voted === true}>
+            Vote down
+          </button>
+        </>
+      ) : (
+        <>
+          <p id="cantVoteMsg">* voting is disabled for own reviews</p>
+        </>
+      )}
     </>
   )
-
 }
 
 export default Voter
